09c7c0185b0be70a2a7c585ea5af2c58be714e9a
alin-grigoras <alan_newgate@icloud.com>
2020-11-24 19:44:33 +0100

    C06 (100% Complete)

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrigora <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/02 12:34:49 by agrigora          #+#    #+#             */
+/*   Updated: 2020/11/02 12:45:35 by agrigora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != 0)
+	{
+		write(1, (str + i), 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
+		i++;
+	return (*(s1 + i) - *(s2 + i));
+}
+
+void	ft_sort_argc(char *tab[], int size)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (ft_strcmp(tab[j], tab[i]) < 0)
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int		main(int argc, char *argv[])
+{
+	int i;
+
+	ft_sort_argc(argv, argc);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+	return (0);
+}
